c5b0a97e8db0e97880c597bcf101cb0f7b450c9e
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2021-06-19 23:18:24 -0700

    add changes

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+    if(document.getElementById("principal").value > 0)
+    {
+        var principal = document.getElementById("principal").value
+
+    }
+    else{
+        return alert("Enter a positive number");
+    }
+
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    var years = Number(document.getElementById("years").value);
+    var totalYears = Number(new Date().getFullYear()) + years;
     var interest = principal * years * rate / 100;
+
+
     document.getElementById("result").innerText  = `
     If you deposit ${principal},
     at an interest rate of ${rate}.
-    You will receive an amount of ${principal + interest},
-    in the year ${years}
+    You will receive an amount of ${interest},
+    in the year ${totalYears}
     `
 }
 
 function rateChange(){
-    document.getElementById("rateValue").innerHTML = `${document.getElementById("rate").value} %` ;
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rateChange").innerHTML = `${document.getElementById("rate").value} %` ;
 }
 
 
